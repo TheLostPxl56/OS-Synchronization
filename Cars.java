55890eb3f8aa69dab9c06ecd58f3d6863bd2121d
TheLostPxl56 <40707617+TheLostPxl56@users.noreply.github.com>
2019-04-24 17:46:19 -0400

    FIXED THAT BLOODY BUG

--- Cars.java
+++ Cars.java
@@ -7,93 +7,80 @@ public class Cars
     {
         Semaphore sem = new Semaphore(1);
 
-        Manager manEven = new Manager(sem,"ahh",0);
-        Manager manOdd = new Manager(sem,"what",1);
+        Manager manEven = new Manager(sem,"Left",0);
+        System.out.println("Left-bound car 0 wants to enter the tunnel.");
+        Manager manOdd = new Manager(sem,"Right",1);
+        System.out.println("Right-bound car 1 wants to enter the tunnel.");
+
+        CarAdder carAdder = new CarAdder(manEven, manOdd);
         //starting up hell tunnel, give them strength
-        manEven.run();
-        manOdd.run();
-
-        int random;
-        //enter infinite loop
-        while(true)
-        {
-            random = (int)(Math.random() * 2);
-
-            if(random == 0)
-            {
-                addCar(manEven, random);
-            }
-            else
-            {
-                addCar(manOdd, random);
-            }
-            random = (int)(Math.random() * 1901 + 100);
-
-            Thread.sleep(random);
-        }
-    }
-
-    //Adds a car to the queue of left or right
-    public static void addCar(Manager manager, int leftRight)
-    {
-        int carNum = manager.queue.get(manager.queue.size() - 1) + 2;
-        manager.queue.add(carNum);
-        //print message
-        if(leftRight == 0)
-        {
-            System.out.println("Left-bound car " + carNum + " wants to enter the tunnel.");
-        }
-        else
-        {
-            System.out.println("Right-bound car " + carNum + " wants to enter the tunnel.");
-        }
+        manEven.start();
+        manOdd.start();
+        carAdder.start();
     }
 }
 //write class that implements runnable
 //this class with
-class Manager implements Runnable{
+class Manager extends Thread{
 
-    Semaphore sem;
+    static Semaphore sem;
     String threadName;
     int counter = 0;
     int num;
-    public ArrayList<Integer> queue = new ArrayList<Integer>();
+    public ArrayList<Integer> queue = new ArrayList<>();
 
     public Manager(Semaphore sem, String threadName,int num) {
         this.sem = sem;
         this.threadName = threadName;
         this.num = num;
         queue.add(num);
+        /*TESTING
+        for(int i = 0; i < 99; i++)
+        {
+            num += 2;
+            queue.add(num);
+        }*/
     }
+
     //Runs for left manager thread
-    private void critLeft(int firstCarIndex)
+    private void critLeft(int firstCarIndex) throws InterruptedException
     {
         //let cars from this side enter the tunnel
-        while(counter < queue.size())
+        while(counter < queue.size() - 1)
         {
             System.out.println("Left-bound car " + queue.get(counter) + " is in the tunnel.");
             counter++;
+            Thread.sleep(1000);
+            if(counter + 1 < queue.size())
+            {
+                continue;
+            }
         }
         //reset counter to the first car that entered the tunnel and let them all leave in sequence
         counter = firstCarIndex;
-        while(counter < queue.size())
+        while(counter < queue.size() - 1)
         {
             System.out.println("Left-bound car " + queue.get(counter) + " has left the tunnel.");
             counter++;
         }
     }
     //Runs for right manager thread
-    private void critRight(int firstCarIndex)
+    private void critRight(int firstCarIndex) throws InterruptedException
     {
         //let cars from this side enter the tunnel
-        while(counter < queue.size())
+        while(counter < queue.size() - 1)
         {
             System.out.println("Right-bound car " + queue.get(counter) + " is in the tunnel.");
             counter++;
+            Thread.sleep(500);
+            if(counter + 1 < queue.size())
+            {
+                continue;
+            }
         }
         //reset counter to the first car that entered the tunnel and let them all leave in sequence
         counter = firstCarIndex;
-        while(counter < queue.size())
+        while(counter < queue.size() - 1)
         {
             System.out.println("Right-bound car " + queue.get(counter) + " has left the tunnel.");
             counter++;
@@ -102,38 +89,106 @@ class Manager implements Runnable{
 
     @Override
     public void run() {
-        int firstCarIndex;
-
-        //infinite loop
-        while(true)
+        try
         {
-            //check queue to see if the counter position exists
-            if(counter < queue.size())
+            int firstCarIndex;
+
+            //infinite loop
+            while (true)
             {
-                //Set firstCar to counter
-                firstCarIndex = counter;
-
-                try
+                //TESTING
+                // System.out.println("Hi, I'm trying to access my queue -" + threadName);
+                //check queue to see if the counter position exists
+                if (counter < queue.size())
                 {
-                    //get semaphore lock
-                    sem.acquire();
+                    //System.out.println(threadName + " has passed its check queuesize check");
 
-                    //ENTER CRITICAL SECTION---------------------------------------
-                    if (threadName.equals("what"))
-                    {
-                        critRight(firstCarIndex);
-                    } else
-                        critLeft(firstCarIndex);
-                    //EXIT CRITICAL SECTION---------------------------------------
+                        //System.out.println(threadName + " is trying to acquire lock");
+                        //get semaphore lock
+                        sem.acquire();
 
-                    //release lock
-                    sem.release();
-                }
-                catch(InterruptedException e)
-                {
-                    System.out.println("Interruption");
+                        //Set firstCar to counter
+                        firstCarIndex = counter;
+
+                        //System.out.println(threadName + " has acquired the lock");
+
+                        //ENTER CRITICAL SECTION---------------------------------------
+                        if (threadName.equals("Right"))
+                        {
+                            critRight(firstCarIndex);
+                        } else
+                            critLeft(firstCarIndex);
+                        //EXIT CRITICAL SECTION---------------------------------------
+
+                        //System.out.println(threadName + " is trying to release the lock");
+                        //release lock
+                        sem.release();
+                        //System.out.println(threadName + " has released the lock");
                 }
             }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong on " + threadName);
+            System.err.println(e);
+        }
+    }
+}
+
+class CarAdder extends Thread
+{
+    Manager left;
+    Manager right;
+
+    public CarAdder(Manager left, Manager right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void run()
+    {
+        int random;
+        //enter infinite loop
+        while(true)
+        {
+            random = (int)(Math.random() * 2);
+
+            if(random == 0)
+            {
+                addCar(left, random);
+            }
+            else
+            {
+                addCar(right, random);
+            }
+            random = (int)(Math.random() * 1901 + 100);
+
+            try
+            {
+                Thread.sleep(random);
+            }
+            catch(Exception e)
+            {
+                System.err.println(e);
+            }
+        }
+    }
+
+    //Adds a car to the queue of left or right
+    public static void addCar(Manager manager, int leftRight)
+    {
+        int carNum = manager.queue.get(manager.queue.size() - 1) + 2;
+        manager.queue.add(carNum);
+
+        //print message
+        if(leftRight == 0)
+        {
+            System.out.println("Left-bound car " + carNum + " wants to enter the tunnel.");
+        }
+        else
+        {
+            System.out.println("Right-bound car " + carNum + " wants to enter the tunnel.");
         }
     }
 }
